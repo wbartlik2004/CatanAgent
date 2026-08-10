7d45fa218e579f1a88e0c2d30ad44b12412b4f37
wbartlik2004 <149321618+wbartlik2004@users.noreply.github.com>
2026-08-10 03:04:44 +0000

    Aug 9, 2026, 11:04 PM

--- main.py
+++ main.py
@@ -66,21 +66,21 @@ def play2(agents, seed=0, verbose=False):
         gs = rules.apply(gs, action)
     return gs
 
-'''if __name__ == "__main__":
+if __name__ == "__main__":
     for seed in (1, 2):
-        agents = [RandomAgent(color="red", seed=seed), HeuristicAgent(color="blue")]
+        agents = [HeuristicAgent(color="red"), HeuristicAgent(color="blue")]
         gs = play2(agents, seed=seed, verbose=True)
         print(f"seed {seed}: winner=P{gs.winner()} "
-              f"VPs={[gs.victory_points(p) for p in range(gs.n)]}")'''
+              f"VPs={[gs.victory_points(p) for p in range(gs.n)]}")
 
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     trainer = CoEvolutionTrainer(population_size=4, n_generations=10, games_per_round=1,
                                  elite_fraction=0.25, tournament_k=3, mutation_rate=0.2,
                                  mutation_sigma=0.25, max_actions_per_game=4000, seed=None)
     trainer.runEvolution(verbose=True)
     print("\n--- Training Complete ---")
     best_weights = trainer.best_genome()
-    print("\nBest Genome Discovered:" + f"\n{best_weights}")
+    print("\nBest Genome Discovered:" + f"\n{best_weights}")'''
     
     
      
