0293994f51779e587204822b0979c067c00db8d6
wbartlik2004 <149321618+wbartlik2004@users.noreply.github.com>
2026-08-10 05:48:41 +0000

    Aug 10, 2026, 1:48 AM

--- agent.py
+++ agent.py
@@ -49,7 +49,7 @@ DEFAULT_WEIGHTS = {
     "victory_points": 10.0,
     "settlements": 4.0,
     "cities": 8.0,
-    "roads": 4.0,
+    "roads": 2.0,
     "resource_total": 0.00,
     "dev_cards": 0.0,
     "longest_road_bonus": 0.0,   
